8fb12cbae200e839143377d1e48dd5e77dc6f150
Felipe-16 <56928106+PAN1NI@users.noreply.github.com>
2021-03-01 11:48:26 -0300

    aula 02

---
+++ aula02/src/Basico01.java
@@ -0,0 +1,23 @@
+package pegini.ros.felipe;
+
+public class Basico01 {
+
+    public static void main(String[] args) {
+        int hp;
+        boolean megaEvolui;
+        float atk;
+        double satk;
+        String nome;
+
+        hp = 100;
+        megaEvolui = true;
+        atk = 50.0f;
+        satk = 55.0;
+        nome = "Gengar";
+
+        System.out.println("Nome:" + nome);
+        System.out.println("Mega Evolui?" +megaEvolui);
+        System.out.println("atk:" + atk + "\nsatk:" + satk);
+
+    }
+}
