8774e4a4d3b50c9a794bc41c0b71125b2eb86a1e
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-03-15 10:16:35 -0300

    Criando um menu com scanner

---
+++ Aula05/src/pegini/ros/felipe/Sistema.java
@@ -0,0 +1,32 @@
+package pegini.ros.felipe;
+
+import java.util.Scanner;
+
+public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
+    public void executar(){
+        int opcao;
+        while (executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
+    }
+
+    public Sistema(){
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in); // Cria um scanner para o teclado
+    }
+
+    private void exibirMenu(){
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1- Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar conta (titulo)");
+        System.out.println("0 - Encerrar sistema");
+
+    }
+}
