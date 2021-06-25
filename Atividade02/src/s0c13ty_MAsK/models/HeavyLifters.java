782ec8cf361700d996b8a25e68f5f25b3848e62b
Juliana-Haroldo <79457732+Juliana-Haroldo@users.noreply.github.com>
2021-06-25 20:40:24 -0300

    Att postarMensagem dos membros

--- Atividade02/src/s0c13ty_MAsK/models/HeavyLifters.java
+++ Atividade02/src/s0c13ty_MAsK/models/HeavyLifters.java
@@ -7,8 +7,16 @@ package s0c13ty_MAsK.models;
 public class HeavyLifters extends Membro {
 
     @Override
-    public String postarMensagem() {
-        return null;
+    public String postarMensagem(Horario horario) {
+
+        String msg1 = "Podem contar conosco!";
+        String msg2 = "N00b_qu3_n_Se_r3pita.bat";
+        if (horario == Horario.NORMAL){
+            return msg1;
+        }
+        else{
+            return msg2;
+        }
     }
 
     @Override
