ffadc2fc62e751eee0aa3cd7abe8f78db37941ec
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-06-26 15:49:49 -0300

    Adicionando um menu para o sistema

--- Atividade02/src/s0c13ty_MAsK/models/BigBrothers.java
+++ Atividade02/src/s0c13ty_MAsK/models/BigBrothers.java
@@ -23,66 +23,7 @@ public class BigBrothers extends Membro{
         super(nome, email, ID, funcao);
     }
 
-    /**
-     * Metodo que cadastra os membros com o seu papel dentro do grupo
-     */
-    public void cadastroMembros () {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Deseja adicionar um novo membro?\n" +
-                "1 - sim \n" +
-                "2 - nao");
-
-        int resposta = Integer.parseInt(scanner.next());
-
-        while (resposta == 1) {
-
-                System.out.println("Que tipo de membro gostaria de adicionar?\n" +
-                        "1 - Mobile Members\n" +
-                        "2 - Heavy Lifters\n" +
-                        "3 - Script Guys \n" +
-                        "4 - Big Brothers");
-                int add = Integer.parseInt(scanner.next());
-
-                System.out.println("Qual o nome do membro");
-                String nome = scanner.next();
-
-                System.out.println("Qual o email?");
-                String email = scanner.next();
-
-                System.out.println("Qual o ID?");
-                int ID = Integer.parseInt(scanner.next());
-
-                switch (add) {
-                    case 1:
-                        MobileMembers novoMobile = new MobileMembers(nome, email, ID, TiposDeMembro.Mobile_Members);
-                        System.out.println(novoMobile.toString());
-                        break;
-
-                    case 2:
-
-                        HeavyLifters novoHeavyLifters = new HeavyLifters(nome, email, ID, TiposDeMembro.Heavy_Lifters);
-                        System.out.println(novoHeavyLifters.toString());
-                        break;
-
-                    case 3:
-
-                        ScriptGuys novoScriptGuy = new ScriptGuys(nome, email, ID, TiposDeMembro.Script_Guys);
-                        System.out.println(novoScriptGuy.toString());
-                        break;
-                    case 4:
-
-                        BigBrothers novoBigBrothers = new BigBrothers(nome, email, ID, TiposDeMembro.Big_Brothers);
-                        System.out.println(novoBigBrothers.toString());
-                        break;
-                }
-
-            System.out.println("Voce deseja cadastrar outro membro?\n" +
-                    "1 - Sim \n" +
-                    "2 - Nao");
-            resposta = Integer.parseInt(scanner.next());
-        }
-    }
 
     /**
      * Método que vai retornar a apresentção do membro
