e48e67624cc86b8167862301f0a451dfee6a43f8
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-06-25 21:23:27 -0300

    update

--- Atividade02/src/s0c13ty_MAsK/controladores/Sistema.java
+++ Atividade02/src/s0c13ty_MAsK/controladores/Sistema.java
@@ -7,7 +7,6 @@ import s0c13ty_MAsK.enumerates.Horario;
  * Classe onde sera executado a implementacao
  */
 public class Sistema {
-    Enum horario = Horario.NORMAL;
     // registrar membros
     // informar quando perguntado o horario
     // postar msg para todos os membros
